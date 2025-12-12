1bd00c33924734793b431367cef7af49cb4c2156
AbdulselamAbdurehman <abdulselamabdurehman@gmail.com>
2025-12-12 08:27:43 +0300

    migrate from next to nuxt

--- app/components/LineBreak.tsx
+++
@@ -1,7 +0,0 @@
-export default function LineBreak() {
-  return (
-    <div className="flex justify-center">
-      <div className="h-0.5 w-2/5 bg-slate-300 text-center rounded"></div>
-    </div>
-  );
-}
